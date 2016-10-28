17a2550d87a669b081ce0aef488c0a751dbc1a15
TheBugMaker <ochi_ghassen@zoho.com>
2016-10-28 17:40:16 +0100

    first commit

---
+++ ConvertToCodingGame/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO; 
+
+namespace ConvertToCodingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Specify File Output Name");
+                return; 
+            }
+
+            HashSet<String> uses = new HashSet<string>(); 
+            var files = Directory.GetFiles(".", "*.cs");
+            LinkedList <String> toWrite = new LinkedList<string>();  
+            foreach (var file in files)
+            {
+                var lines = File.ReadAllLines (file);
+                foreach (var line in lines)
+                {
+                    if (line.Contains("using "))
+                    {
+                        uses.Add(line.ToString());
+                    }
+                    else
+                    {
+                        toWrite.AddLast(line); 
+                    }
+                }
+            }
+            foreach (var use in uses)
+            {
+                toWrite.AddFirst(use); 
+            }
+
+            File.WriteAllLines((String)args[0] , toWrite.ToArray<String>()); 
+        }
+    }
+}
